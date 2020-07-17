809a3be176d1ad32cab5fa4d7afbe306c956d6fe
Eduard <artemshykuri98@gmail.com>
2020-07-17 11:29:21 -0400

    removed twillio sid info

--- send-sms.php
+++ send-sms.php
@@ -7,8 +7,8 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$from = "+12034336067";//$_POST['from'];
-$to = "+14325867651";//$_POST['to'];
+$from = "+";//$_POST['from'];
+$to = "+1";//$_POST['to'];
 $body = "asdadasd";//$_POST['body'];
 print($from);
 print($to);
@@ -16,8 +16,8 @@ $callbackURL = "https://f8f718775bd3.ngrok.io/twillio-handler-demo/twilio-sms-st
 
 // Find your Account Sid and Auth Token at twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-$sid    = "AC6947a4bb01b06c0cb4bec795a30869c1";
-$token  = "8c3531aaa8ea644f87730e5110415609";
+$sid    = "your sid";
+$token  = "your token";
 $twilio = new Client($sid, $token);
 print($twilio);
 
